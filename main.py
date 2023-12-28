1a9ec4b18622b27803c381d91a0154024c6df3a2
Michael Primavera <pastasauce59@gmail.com>
2023-12-27 23:07:45 -0500

    Successful post request to nutrionix APIthat gives a json response.

--- main.py
+++ main.py
@@ -1,1 +1,16 @@
 from keys import *
+import requests
+
+api_endpoint = "https://trackapi.nutritionix.com/v2/natural/exercise"
+
+headers = {
+    "x-app-id": APP_ID,
+    "x-app-key": API_KEY
+}
+
+exercise_config = {
+    "query": input("Tell me which exercises you did. ")
+}
+
+response =  requests.post(url=api_endpoint, json=exercise_config, headers=headers)
+print(response.text)
